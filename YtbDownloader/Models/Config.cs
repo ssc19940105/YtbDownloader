bc795e08c10680b90c5929764353f60bb8937292
沈舒超 <ssc19940105@163.com>
2020-01-08 23:57:24 +0800

    Revert "Refine Code"
    This reverts commit 33e4e887cc011936ad2eeee4467ce686fee8f1e5.

--- YtbDownloader/Models/Config.cs
+++ YtbDownloader/Models/Config.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.ComponentModel;
 using YtbDownloader.Core.Interfaces;
 
 namespace YtbDownloader.Models
 {
-    public class Config : IConfig
+    public class Config : INotifyPropertyChanged, IConfig
     {
+#pragma warning disable CS0067
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+#pragma warning restore CS0067
+
         public bool IsProxy { get; set; }
 
         public Uri ProxyUrl { get; set; }
